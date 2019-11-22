57d7a4b0fa495ae6d6dfb2beabe8eecd6f300db3
David Lin <yoxisem544@gmail.com>
2019-11-22 18:47:35 +0800

    - bump version 1.0.2 to fix file - add test case

---
+++ Example/Tests/DecodingSpecs.swift
@@ -0,0 +1,15 @@
+//
+//  DecodingSpecs.swift
+//  APIKit_Tests
+//
+//  Created by David on 2019/11/22.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import Quick
+import Nimble
+import APIKit
+
+class DecodingSpecs: QuickSpec {
+
+}
